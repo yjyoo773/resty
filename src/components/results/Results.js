778b18ab62e334195fc3410d61eb5ee9578b94de
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-05-25 18:00:32 -0700

    minor changes for result

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -1,17 +1,14 @@
 import React from 'react'
+import ReactJson from 'react-json-view'
 import './results.scss'
 
 class Results extends React.Component{
     
     render(){
-        let body = this.props.results[1]
-        let header = this.props.results[0]
-        console.log(this.props.results)
         return (
-            <>
-                <pre>{JSON.stringify(header,null,2)} {"\n"}
-                {JSON.stringify(body,null,2)}</pre>
-            </>
+            <div className='results'>
+                <ReactJson src={this.props.results}/>
+            </div>
         )
     }
 }
